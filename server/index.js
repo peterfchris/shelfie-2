4e0cd4139e0c73ced54ebca63729ff1c2f208d97
Peter Christensen <peter.f.chris@gmail.com>
2019-05-22 20:38:50 -0600

    part 1 step 3 done

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const massive = require('massive')
 const app = express()
 app.use(express.json())
 const {SERVER_PORT, CONNECTION_STRING} = process.env
+const controller = require('./controller')
 
 
 massive(CONNECTION_STRING).then((database) => {
@@ -11,3 +12,5 @@ massive(CONNECTION_STRING).then((database) => {
     console.log('database set!')
     app.listen(SERVER_PORT, () => console.log(`Nothing is broken yet on ${SERVER_PORT}`))
 })
+
+app.get('/api/inventory', controller.getInventory)
